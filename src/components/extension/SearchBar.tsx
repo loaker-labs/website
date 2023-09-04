1ed7aba89a334fa3545a549d8cd69bac710878a9
jbcol <jeanbaptistecolj@gmail.com>
2023-09-04 19:08:31 +0200

    logo

--- src/components/extension/SearchBar.tsx
+++
@@ -1,5 +0,0 @@
-export const SearchBar = () => {
-    return (
-        <input className="flex rounded-xl bg-neutral-200 text-black focus:outline-none pl-2" type="text" placeholder="Search coming soon"/>
-    );
-};
